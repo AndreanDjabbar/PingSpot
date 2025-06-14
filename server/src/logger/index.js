d4e5a538f0ead05591542dbb38a3b40c71c45306
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-06-14 16:48:36 +0700

    Add: create custom logger initialization

---
+++ server/src/logger/index.js
@@ -0,0 +1,45 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import pino from 'pino';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const loggerInfo = process.env.LOGGER_LEVEL || 'info';
+
+const fileTransport = pino.transport({
+  target: 'pino-roll',
+  options: {
+    file: join(__dirname, 'app.log'),
+    frequency: 'daily',
+    mkdir: true,
+    size: '10M',
+    keep: 7,
+    compress: true,
+  }
+});
+
+const prettyTransport = pino.transport({
+  target: 'pino-pretty',
+  options: {
+    colorize: true,
+    translateTime: 'SYS:standard',
+    ignore: 'pid,hostname'
+  }
+});
+
+const logger = pino(
+  {
+    level: loggerInfo,
+    formatters: {
+      level(label) {
+        return { level: label };
+      }
+    }
+  },
+  pino.multistream([
+    { stream: prettyTransport },
+    { stream: fileTransport }
+  ])
+);
+
+export default logger;
