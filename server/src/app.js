8c53ce8b4f90e89d66fdb54995cec8750b405f03
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-06-14 18:25:35 +0700

    Add: add connection for supabase postgre and prisma ORM

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import mainRoutes from './routes/mainRoutes.js';
+import prisma from './config/DBConfig.js';
 
 const app = Fastify({logger: false});
 app.register(mainRoutes, { prefix: '/api' });
