bfa0d8601601e1ed84c5810fc8106f2fd46b820b
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-07-20 14:57:01 +0700

    Update: update background theme position

--- client/src/components/BackgroundTheme.tsx
+++ client/src/components/BackgroundTheme.tsx
@@ -9,13 +9,13 @@ const BackgroundTheme: React.FC<BackgroundThemeProps> = ({
 }) => (
     <div className={`${className} w-full h-full bg-pingspot-gradient flex items-center justify-center`}>
         <div className="absolute inset-0 bg-black/20"></div>
-        <div className="relative z-10 text-white text-center p-8">
-        <h2 className="text-4xl font-bold mb-4">PingSpot</h2>
-        <p className="text-xl opacity-90">Real-time community-powered platform for reporting and tracking local issues on an interactive map.</p>
-        <div className='mt-5'>
-            <div className="w-full h-px bg-white/10 my-2"></div>
-            <p className="text-white/70 text-xs">&copy; 2025 PingSpot. All rights reserved.</p>
-        </div>
+        <div className="absolute top-70 text-white text-center p-8">
+            <h2 className="text-4xl font-bold mb-4">PingSpot</h2>
+            <p className="text-xl opacity-90">Platform komunitas real-time untuk melaporkan dan memantau permasalahan lokal pada peta interaktif.</p>
+            <div className='mt-5'>
+                <div className="w-full h-px bg-white/10 my-2"></div>
+                <p className="text-white/70 text-xs">&copy; 2025 PingSpot. Hak cipta dilindungi.</p>
+            </div>
         </div>
     </div>
 );
