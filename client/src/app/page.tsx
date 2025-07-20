3f08453ae46b65906898a9e69766467c0158c421
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-07-20 14:56:41 +0700

    Update: update auth layout height

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div className="">
-      <div className="flex">
+      <div className="flex flex-col">
         <h1 className="text-2xl font-bold">Welcome to Pingspot</h1>
+        <div className="mt-3 flex w-full justify-center gap-5">
+          <a href="/auth/login" className="bg-sky-700 p-4 text-white rounded-lg">Login</a>
+          <a href="/auth/register" className="bg-sky-700 p-4 text-white rounded-lg">Register</a>
+        </div>
       </div>
     </div>
   );
--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -5,7 +5,7 @@ const AuthLayout: React.FC<React.PropsWithChildren> = ({
     children
 }) => {
     return (
-        <div className="flex h-screen">
+        <div className="flex min-h-screen">
             <div className="w-full px-2 sm:px-8 md:px-15 lg:px-10 lg:w-1/2">
                 <div className='pt-2'>
                     <PingspotLogo />
