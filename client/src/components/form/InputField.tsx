b7548e47258708d2dc8d41ffd30da056adf9953e
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-07-22 17:59:28 +0700

    Update: add on change props in input field

--- client/src/components/form/InputField.tsx
+++ client/src/components/form/InputField.tsx
@@ -12,6 +12,9 @@ interface InputFieldProps {
     icon: React.ReactNode;
     placeHolder?: string;
     showPasswordToggle?: boolean;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
@@ -24,9 +27,13 @@ const InputField: React.FC<InputFieldProps> = ({
     withLabel = true,
     labelTitle = '',
     showPasswordToggle = false,
-    icon
+    icon,
+    value,
+    onChange,
+    onBlur
 }) => {
     const [showPassword, setShowPassword] = useState(false);
+    const inputType = type === 'password' && showPasswordToggle ? (showPassword ? 'text' : 'password') : type;
     return (
         <div className={`space-y-1 ${className}`}>
             {withLabel && (
@@ -41,10 +48,13 @@ const InputField: React.FC<InputFieldProps> = ({
                 <input
                     id={id}
                     name={name}
-                    type={type}
+                    type={inputType}
                     required={required}
                     className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-800 focus:border-sky-800 transition-all duration-200"
-                    placeholder= {placeHolder || `Masukkan ${labelTitle.toLowerCase()}`}
+                    placeholder={placeHolder || `Masukkan ${labelTitle.toLowerCase()}`}
+                    value={value}
+                    onChange={onChange}
+                    onBlur={onBlur}
                 />
                 {type === 'password' && showPasswordToggle && (
                     <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
