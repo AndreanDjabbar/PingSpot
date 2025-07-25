ff2b7f204420b472929d8070da09c5a0384c19db
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-07-25 16:33:14 +0700

    Update: update styling in background layout

--- client/src/components/UI/BackgroundTheme.tsx
+++ client/src/components/UI/BackgroundTheme.tsx
@@ -7,9 +7,9 @@ interface BackgroundThemeProps {
 const BackgroundTheme: React.FC<BackgroundThemeProps> = ({ 
     className 
 }) => (
-    <div className={`${className} w-full h-full bg-pingspot-gradient flex items-center justify-center`}>
+    <div className={`${className} w-full h-full bg-pingspot-gradient flex items-center justify-start fixed right-0 left-1/2`}>
         <div className="absolute inset-0 bg-black/20"></div>
-        <div className="absolute top-70 text-white text-center p-8">
+        <div className="text-white text-center w-1/2 p-5">
             <h2 className="text-4xl font-bold mb-4">PingSpot</h2>
             <p className="text-xl opacity-90">Platform komunitas real-time untuk melaporkan dan memantau permasalahan lokal pada peta interaktif.</p>
             <div className='mt-5'>
