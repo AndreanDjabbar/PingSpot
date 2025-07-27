32a3076de6dee0c92386aa334d33056b9ff42132
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-07-27 15:08:50 +0700

    Update: add interface for promise data

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IRegisterFormType } from "@/app/auth/types";
 import axios from "axios";
 
 const AUTH_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/auth`;
 
-export const registerService = async (payload: IRegisterFormType) => axios.post(`${AUTH_API_URL}/register`, payload)
+type IResponseType = {
+    message: string;
+    data?: any;
+}
+
+export const registerService = async (payload: IRegisterFormType): Promise<IResponseType> => {
+    const response = await axios.post<IResponseType>(`${AUTH_API_URL}/register`, payload);
+    return response.data;
+};
