a4719d5fd0f652242e28a6100223fec2b4babedb
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-02 23:01:25 +0700

    Update: add success section

--- client/src/app/auth/google/page.tsx
+++ client/src/app/auth/google/page.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
 import AuthLayout from '@/layouts/AuthLayout'
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useToast } from '@/hooks/useToast';
+import SuccessSection from '@/components/UI/SuccessSection';
 
 const GoogleAuthpage = () => {
+    const searchParams = useSearchParams();
+    const token = searchParams.get('token');
+    const router = useRouter();
+    const { toastSuccess } = useToast();
+
+    useEffect(() => {
+        if (token) {
+            toastSuccess('Akun berhasil diverifikasi');
+            setTimeout(() => {
+                router.push("/");
+            }, 2000);
+        }
+    }, [token, toastSuccess]);
+    
     return (
         <AuthLayout>
-            WELCOMEEE TO GOOGLE AUTH PAGE
+            <div className="space-y-8">
+                <div className="text-center space-y-1">
+                    <h1 className="text-3xl font-bold text-sky-800">Verifikasi Akun Google</h1>
+                    <p className="text-sky-800">Kami akan memverifikasi akun anda</p>
+                </div>
+                {token && (
+                    <SuccessSection message="Akun berhasil diverifikasi melalui Google.." />
+                )}
+            </div>
         </AuthLayout>
-    )
+    );
 }
 
 export default GoogleAuthpage
