77f508948b375a4911e75a1205b5c5b83f8e1c0f
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-02 19:45:11 +0700

    Add: create temporary google auth page

---
+++ client/src/app/auth/google/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthLayout from '@/layouts/AuthLayout'
+
+const GoogleAuthpage = () => {
+    return (
+        <AuthLayout>
+            WELCOMEEE TO GOOGLE AUTH PAGE
+        </AuthLayout>
+    )
+}
+
+export default GoogleAuthpage
