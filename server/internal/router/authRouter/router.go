b6f4a2de4340b012991cd471b0cde5b20395e7d6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-02 19:43:41 +0700

    Add: add route for google auth and google callback

--- server/internal/router/authRouter/router.go
+++ server/internal/router/authRouter/router.go
@@ -3,6 +3,7 @@ package authrouter
 import (
 	authhandler "server2/internal/handler/authHandler"
 
+	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,4 +13,6 @@ func RegisterAuthRoutes(app *fiber.App) {
 	authRoute.Post("/verification", authhandler.VerificationHandler)
 	authRoute.Post("/register", authhandler.RegisterHandler)
 	authRoute.Post("/login", authhandler.LoginHandler)
+	authRoute.Get("/google", adaptor.HTTPHandlerFunc(authhandler.GoogleLoginHandler))
+	authRoute.Get("/google/callback", adaptor.HTTPHandlerFunc(authhandler.GoogleCallbackHandler))
 }
