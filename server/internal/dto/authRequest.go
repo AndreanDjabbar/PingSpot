75d280e8913c37e507a615b56f3c3d0145d8d9d1
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-05 14:47:51 +0700

    Add: create dto request for forgot password email verification

--- server/internal/dto/authRequest.go
+++ server/internal/dto/authRequest.go
@@ -14,3 +14,7 @@ type LoginRequest struct {
 	Email      string  `json:"email" validate:"required,email"`
 	Password   string  `json:"password" validate:"required,min=6"`
 }
+
+type ForgotPasswordEmailVerificationRequest struct {
+	Email string `json:"email" validate:"required,email"`
+}
