d8dc2b84a3d9d14795c3f3cf17d816f5580da36e
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-05 14:44:55 +0700

    Add: create API route for forgot password email verifcation

--- server/internal/router/authRouter/router.go
+++ server/internal/router/authRouter/router.go
@@ -13,6 +13,7 @@ func RegisterAuthRoutes(app *fiber.App) {
 	authRoute.Post("/verification", authhandler.VerificationHandler)
 	authRoute.Post("/register", authhandler.RegisterHandler)
 	authRoute.Post("/login", authhandler.LoginHandler)
+	authRoute.Post("/forgot-password/email-verification", authhandler.ForgotPasswordEmailVerificationHandler)
 	authRoute.Get("/google", adaptor.HTTPHandlerFunc(authhandler.GoogleLoginHandler))
 	authRoute.Get("/google/callback", adaptor.HTTPHandlerFunc(authhandler.GoogleCallbackHandler))
 }
