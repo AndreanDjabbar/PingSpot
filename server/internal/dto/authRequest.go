15f1327832a25bf9dabad60630ba2c334908269d
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-05 17:13:42 +0700

    Add: create reset password dto

--- server/internal/dto/authRequest.go
+++ server/internal/dto/authRequest.go
@@ -18,3 +18,9 @@ type LoginRequest struct {
 type ForgotPasswordEmailVerificationRequest struct {
 	Email string `json:"email" validate:"required,email"`
 }
+
+type ForgotPasswordResetPasswordRequest struct {
+	Password string `json:"password" validate:"required,min=6"`
+	PasswordConfirmation string `json:"passwordConfirmation" validate:"required,eqfield=Password"`
+	Email string `json:"email" validate:"required,email"`
+}
