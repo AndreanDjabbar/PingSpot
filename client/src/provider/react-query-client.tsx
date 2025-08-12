c57cef0c408fdeeae0d143ca7466d453098f39a3
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-12 13:08:03 +0700

    Update: use arrow function instead

--- client/src/provider/react-query-client.tsx
+++ client/src/provider/react-query-client.tsx
@@ -3,8 +3,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
-export function ReactQueryClientProvider({ children }: { children: React.ReactNode }) {
+const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
     const [queryClient] = useState(() => new QueryClient());
 
     return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
+
+export { ReactQueryClientProvider };
