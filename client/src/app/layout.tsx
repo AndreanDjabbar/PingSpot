d8ab7c16f1ac9b0ef147d6f562bff33e527cf5ba
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-12 14:05:21 +0700

    Add: add SF Pro fonts

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import "./globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ReactQueryClientProvider } from "@/provider/react-query-client";
 import type { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "PingSpot",
@@ -26,7 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`antialiased`}
+      style={{ fontFamily: 'var(--font-sf)' }}>
         <ReactQueryClientProvider>
           <ToastContainer/>
           {children}
