3fbfb1ee7d5103f64dfb71eaa347cba40a93140a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-12 14:19:21 +0700

    Update: add google client component to seperate

--- client/src/app/auth/google/page.tsx
+++ client/src/app/auth/google/page.tsx
@@ -1,43 +1,12 @@
-"use client";
-import React, { useEffect } from 'react'
-import AuthLayout from '@/layouts/AuthLayout'
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useToast } from '@/hooks/useToast';
-import SuccessSection from '@/components/UI/SuccessSection';
+import GoogleAuthClient from "./GoogleClient";
+import { Suspense } from "react";
 
-const GoogleAuthpage = () => {
-    const searchParams = useSearchParams();
-    const token = searchParams.get('token');
-    const router = useRouter();
-    const { toastSuccess } = useToast();
-
-    useEffect(() => {
-        if (token) {
-            const payload = JSON.parse(atob(token.split('.')[1]));
-            const jwtExpiration = payload.exp || 0;
-            if (token) {
-                document.cookie = `auth_token=${token}; path=/; expires=${new Date(jwtExpiration * 1000).toUTCString()}; secure; samesite=strict`;
-            }
-            setTimeout(() => {
-                router.push("/main");
-            }, 2000);
-            toastSuccess('Akun berhasil diverifikasi');
-        }
-    }, [token, toastSuccess, router]);
-    
+const GoogleAuthPage = () => {
     return (
-        <AuthLayout>
-            <div className="space-y-8">
-                <div className="text-center space-y-1">
-                    <h1 className="text-3xl font-bold text-sky-800">Verifikasi Akun Google</h1>
-                    <p className="text-sky-800">Kami akan memverifikasi akun anda</p>
-                </div>
-                {token && (
-                    <SuccessSection message="Akun berhasil diverifikasi melalui Google.." />
-                )}
-            </div>
-        </AuthLayout>
+        <Suspense fallback={<div>Loading...</div>}>
+            <GoogleAuthClient />
+        </Suspense>
     );
 }
 
-export default GoogleAuthpage
+export default GoogleAuthPage;
