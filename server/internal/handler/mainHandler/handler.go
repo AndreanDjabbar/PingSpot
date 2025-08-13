04b66c24827770f317ce09d5d2ee307e55dcdce3
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-13 16:53:06 +0700

    Update: add usage of get jwt claims utility function example

--- server/internal/handler/mainHandler/handler.go
+++ server/internal/handler/mainHandler/handler.go
@@ -3,13 +3,20 @@ package mainHandler
 import (
 	"server2/internal/logger"
 	"server2/pkg/utils/envUtils"
+	mainutils "server2/pkg/utils/mainUtils"
 	"server2/pkg/utils/responseUtils"
 
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func DefaultHandler(c *fiber.Ctx) error {
 	logger.Info("DEFAULT MAIN HANDLER")
+	_, error := mainutils.GetJWTClaims(c)
+	if error != nil {
+		logger.Error("Failed to get JWT claims", zap.Error(error))
+		return responseUtils.ResponseError(c, 401, "Token tidak valid", "", "Anda harus login terlebih dahulu")
+	}
 	data := map[string]any{
 		"message":    "Selamat datang di Pingspot MAIN API.. Silakan cek repository untuk informasi lebih lanjut.",
 		"repository": envUtils.GithubRepoURL(),
