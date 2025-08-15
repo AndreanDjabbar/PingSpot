89c1980e15403a5bd7185af7bb95ad0956e3434a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-15 10:07:59 +0700

    Update: use the reversed location data from location store

--- client/src/app/main/components/Map.tsx
+++ client/src/app/main/components/Map.tsx
@@ -5,6 +5,9 @@ import { getErrorResponseDetails, getErrorResponseMessage } from '@/utils/gerErr
 import React, { useEffect } from 'react'
 import { useReverseCurrentLocation } from '@/hooks/main/useReverseCurrentLocation'
 import { FaMap, FaMapPin, FaSpinner } from 'react-icons/fa'
+import { getDataResponseDetails } from '@/utils/getDataResponse'
+import { useLocationStore } from '@/stores/userLocationStore'
+import camelize from 'camelize';
 
 interface ReverseLocationResponse {
     display_name?: string;
@@ -20,19 +23,40 @@ interface ReverseLocationResponse {
 
 const Map = () => {
     const { mutate: reverseLocation, data, isPending: reverseLoading } = useReverseCurrentLocation();
-    const { location, isError, error, loading, permissionDenied, isPermissionDenied, requestLocation } = useCurrentLocation();
+    const { location, isError, error, loading, permissionDenied, isPermissionDenied, requestLocation, isUpdateRequest } = useCurrentLocation();
+    const setLocationStore = useLocationStore((state) => state.setLocation);
     
     useErrorToast(isError, error);
     useErrorToast(isPermissionDenied, permissionDenied);
 
     useEffect(() => {
         if (location) {
-            reverseLocation({
-                latitude: location.lat.toString(),
-                longitude: location.lng.toString()
+            if (!location.displayName || !location.address || isUpdateRequest) {
+                reverseLocation({
+                    latitude: location.lat,
+                    longitude: location.lng
+                });
+            }
+        }
+    }, [location?.lat, location?.lng, location?.lastUpdated, reverseLocation, isUpdateRequest]);
+
+    useEffect(() => {
+        if (data) {
+            const locationData = camelize(getDataResponseDetails(data));
+            setLocationStore({
+                ...location,
+                lat: location?.lat ?? "",
+                lng: location?.lng ?? "",
+                lastUpdated: location?.lastUpdated || new Date().toLocaleString(),
+                displayName: locationData.displayName || locationData.display_name,
+                address: locationData.address || null,
+                type: locationData.type || null,
+                name: locationData.name || null,
+                osmType: locationData.osmType || null,
+                osmId: locationData.osmId || null,
             });
         }
-    }, [location, reverseLocation]);
+    }, [data, setLocationStore]);
 
     return (
         <div className="bg-white/80 backdrop-blur-sm rounded-xl border border-white/50 shadow-lg p-6 flex flex-col gap-6">
@@ -54,7 +78,7 @@ const Map = () => {
                         <button
                             className="bg-sky-600 text-white hover:bg-sky-700 px-6 py-2 rounded-lg transition-colors"
                             disabled={loading}
-                            onClick={requestLocation}
+                            onClick={() => requestLocation(false)}
                         >
                             {loading ? (
                                 <div className='flex items-center gap-2'>
@@ -71,7 +95,7 @@ const Map = () => {
                         <div className='flex flex-col items-center gap-2'>
                             <button 
                                 className="bg-sky-800 flex items-center px-6 py-2 gap-2 text-white rounded-lg hover:bg-sky-900 transition-colors"
-                                onClick={requestLocation}
+                                onClick={() => requestLocation(true)}
                                 disabled={loading}
                             >
                                 {loading ? (
@@ -105,20 +129,20 @@ const Map = () => {
                             <FaSpinner className="w-3 h-3 animate-spin" />
                             <span className="text-xs">Mencari alamat...</span>
                         </div>
-                    ) : data ? (
+                    ) : data || location.displayName || location.address ? (
                         <div className="text-xs text-green-600 flex flex-col gap-2">
                             <p className="flex items-center gap-1 p-1 text-green-700 font-semibold">
                             <FaMapPin className="text-red-500" />
                             Lokasi Anda:
                             </p>
                             <div className="">
-                                {(data as ReverseLocationResponse)?.display_name || 'Alamat tidak ditemukan'}
+                                {(data as ReverseLocationResponse)?.display_name || location.displayName || 'Alamat tidak ditemukan'}
                             </div>
                         </div>
                     ) : (
                         <div className="text-xs text-green-600">
-                            <div>Lat: {location?.lat.toFixed(6) || ""}</div>
-                            <div>Lng: {location?.lng.toFixed(6) || ""}</div>
+                            <div>Lat: {location?.lat || ""}</div>
+                            <div>Lng: {location?.lng || ""}</div>
                         </div>
                     )}
                 </div>
