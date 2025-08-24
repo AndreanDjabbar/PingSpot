1e5438b9bcf2c730757315d3496e4cc2d8fdc9db
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-24 14:52:05 +0700

    Update: separate redis config logic and redis init logic

--- server/internal/config/redisConfig.go
+++ server/internal/config/redisConfig.go
@@ -1,44 +1,18 @@
 package config
 
-import (
-	"context"
-	"fmt"
-	"server2/internal/logger"
-	"server2/pkg/utils/envUtils"
-	"sync"
-	"time"
+import "server2/pkg/utils/envUtils"
 
-	"github.com/redis/go-redis/v9"
-	"go.uber.org/zap"
-)
-
-var (
-	redisInstance *redis.Client
-	redisOnce     sync.Once
-	redisErr      error
-)
-
-func InitRedis() error {
-	redisOnce.Do(func() {
-		addr := fmt.Sprintf("%s:%s", envUtils.RedisHost(), envUtils.RedisPort())
-		opt := &redis.Options{
-			Addr:     addr,
-			DB:       0,
-		}
-		redisInstance = redis.NewClient(opt)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		if err := redisInstance.Ping(ctx).Err(); err != nil {
-			logger.Error("Failed to connect to Redis", zap.Error(err))
-			redisErr = fmt.Errorf("failed to connect to Redis: %w", err)
-		} else {
-			logger.Info("Connected to Redis successfully")
-		}
-	})
-	return redisErr
+type RedisConfig struct {
+    Host     string
+    Port     string
+    Password string
+    DB       int
 }
 
-func GetRedis() *redis.Client {
-	return redisInstance
+func LoadRedisConfig() RedisConfig {
+    return RedisConfig{
+        Host:     envUtils.RedisHost(),
+        Port:     envUtils.RedisPort(),
+        DB:       0,
+    }
 }
