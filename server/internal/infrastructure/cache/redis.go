5c67ee18f50a22eda332557d3ba04397e0ea93f8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-24 14:51:05 +0700

    Add: add infrastructure module

---
+++ server/internal/infrastructure/cache/redis.go
@@ -0,0 +1,40 @@
+package cache
+
+import (
+    "context"
+    "fmt"
+    "time"
+
+    "server2/internal/config"
+    "server2/internal/logger"
+
+    "github.com/redis/go-redis/v9"
+    "go.uber.org/zap"
+)
+
+var redisInstance *redis.Client
+
+func InitRedis(cfg config.RedisConfig) (error) {
+    addr := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
+
+    client := redis.NewClient(&redis.Options{
+        Addr:     addr,
+        DB:       cfg.DB,
+    })
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    if err := client.Ping(ctx).Err(); err != nil {
+        logger.Error("Failed to connect to Redis", zap.Error(err))
+        return fmt.Errorf("failed to connect to Redis: %w", err)
+    }
+
+    logger.Info("Connected to Redis successfully")
+    redisInstance = client
+    return nil
+}
+
+func GetRedis() *redis.Client {
+    return redisInstance
+}
