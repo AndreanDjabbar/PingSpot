cbaf1342feda2b9cf19b400f4e1440f9324401c3
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-24 20:34:31 +0700

    Update: update dto request and separate it in each context

---
+++ server/internal/dto/auth/registerRequest.go
@@ -0,0 +1,11 @@
+package auth
+
+type RegisterRequest struct {
+	Username   string  `json:"username" validate:"required,min=3"`
+	Email      string  `json:"email" validate:"required,email"`
+	Password   string  `json:"password" validate:"required,min=6"`
+	FullName   string  `json:"fullName" validate:"required"`
+	Phone      string  `json:"phone" validate:"required,phoneid"`
+	Provider   string  `json:"provider" validate:"required,oneof=EMAIL GOOGLE GITHUB"`
+	ProviderID *string `json:"providerId"`
+}
