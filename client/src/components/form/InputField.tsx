a3a9e886397ee6334a5c1c3ce09f75df35e1aa2c
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-27 13:26:38 +0700

    Update: update icon color

--- client/src/components/form/InputField.tsx
+++ client/src/components/form/InputField.tsx
@@ -44,7 +44,7 @@ const InputField: React.FC<InputFieldProps> = ({
                 </label>
             )}
             <div className='relative'>
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-sky-800">
                     {icon}
                 </div>
                 <input
