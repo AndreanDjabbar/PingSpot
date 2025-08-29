e293c87c6809602b1b7a20cdff47759007b9ba03
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-08-29 10:56:30 +0700

    Update: separate client component

---
+++ client/src/app/client-layout.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+
+import { useEffect } from "react";
+import { useUserProfileStore } from "@/stores/userProfileStore";
+import { useGlobalStore } from "@/stores/globalStore";
+import { useLocationStore } from "@/stores/userLocationStore";
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+    const loadUser = useUserProfileStore((state) => state.loadUser);
+    const globalStore = useGlobalStore();
+    const locationStore = useLocationStore();
+
+    useEffect(() => {
+        const now = Date.now();
+        if (globalStore.expiredAt && now > globalStore.expiredAt) {
+        globalStore.clearGlobalData();
+        }
+
+        if (locationStore.location?.expiresAt && now > locationStore.location.expiresAt) {
+        locationStore.clearLocation();
+        }
+    }, []);
+
+    useEffect(() => {
+        loadUser();
+    }, [loadUser]);
+
+    return <>{children}</>;
+};
+
+export default ClientLayout;
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,40 +1,26 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-"use client";
-
 import "./globals.css";
 import { ReactQueryClientProvider } from "@/provider/react-query-client";
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
-import { useUserProfileStore } from "@/stores/userProfileStore";
-import { useGlobalStore } from "@/stores/globalStore";
-import { useLocationStore } from "@/stores/userLocationStore";
+import ClientLayout from "./client-layout";
+
+export const metadata = {
+  title: 'PingSpot',
+  icons: {
+    icon: '/favicon.png'
+  }
+}
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const loadUser = useUserProfileStore((state) => state.loadUser);
-  const globalStore = useGlobalStore();
-  const locationStore = useLocationStore();
-
-  useEffect(() => {
-    const now = Date.now();
-    if (globalStore.expiredAt && now > globalStore.expiredAt) {
-      globalStore.clearGlobalData();
-    }
-
-    if (locationStore.location?.expiresAt && now > locationStore.location.expiresAt) {
-      locationStore.clearLocation();
-    }
-  }, []);
-
-  useEffect(() => {
-    loadUser();
-  }, [loadUser]);
 
   return (
     <html lang="en">
       <body className="antialiased" style={{ fontFamily: "var(--font-sf)" }}>
         <ReactQueryClientProvider>
           <ToastContainer />
-          {children}
+            <ClientLayout>
+              {children}
+            </ClientLayout>
         </ReactQueryClientProvider>
       </body>
     </html>
