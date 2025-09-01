dd278d6892e21e445eba79dfc2a6eda0cda8538d
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-01 15:18:35 +0700

    Add: create save profile service

--- server/internal/service/user/service.go
+++ server/internal/service/user/service.go
@@ -154,3 +154,57 @@ func UpdateUserByEmail(db *gorm.DB, email string, updatedUser *userModel.User) (
 
 	return &user, nil
 }
+
+func SaveProfile(db *gorm.DB, userID uint, req userDto.SaveUserProfileRequest) (*userModel.UserProfile, error) {
+	tx := db.Begin()
+	if tx.Error != nil {
+		return nil, errors.New("Gagal memulai transaksi")
+	}
+
+	if err := tx.Model(&userModel.User{}).
+		Where("id = ?", userID).
+		Updates(map[string]interface{}{
+			"full_name": req.FullName,}).Error; err != nil {
+			tx.Rollback()
+			return nil, errors.New("Gagal memperbarui data user")
+	}
+	var profile userModel.UserProfile
+	if err := tx.Where("user_id = ?", userID).First(&profile).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			profile = userModel.UserProfile{
+				UserID: userID,
+				Bio:    req.Bio,
+				Avatar: req.Avatar,
+				Age: func() uint { 
+						if req.Age != nil {
+							return *req.Age
+						} else {
+							return 0
+						}
+					}(),
+			}
+			if err := tx.Create(&profile).Error; err != nil {
+				tx.Rollback()
+				return nil, errors.New("Gagal membuat profil user")
+			}
+		} else {
+			tx.Rollback()
+			return nil, errors.New("Gagal mengambil data profil user")
+		}
+	} else {
+		if err := tx.Model(&profile).Updates(map[string]interface{}{
+			"bio":    req.Bio,
+			"avatar": req.Avatar,
+			"age":	req.Age,
+			"gender": req.Gender,
+		}).Error; err != nil {
+			tx.Rollback()
+			return nil, errors.New("Gagal memperbarui profil user")
+		}
+	}
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return nil, errors.New("Gagal menyimpan perubahan")
+	}
+	return &profile, nil
+}
