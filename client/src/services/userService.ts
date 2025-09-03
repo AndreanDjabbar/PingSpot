e0daaaa9e6e087a3f75232e6f8119d5117b4fc99
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-03 10:28:32 +0700

    add: create retrieve my profile data service

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,12 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { IForgotPasswordFormEmailType, IForgotPasswordResetPasswordType, ILoginFormType, ILogoutType, IRegisterFormType, IVerificationType } from "@/types/authTypes";
+import getAuthToken from "@/utils/getAuthToken";
 import axios from "axios";
 
 const USER_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/user`;
+const AUTH_TOKEN = getAuthToken() || "";
 
 type IResponseType = {
     message: string;
     data?: any;
+}
+
+const headers = (authToken: string) => {
+    return {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${authToken}`
+        }
+    }
 }
 
 export const registerService = async (payload: IRegisterFormType): Promise<IResponseType> => {
@@ -47,3 +59,8 @@ export const resetPasswordService = async (payload: IForgotPasswordResetPassword
     const response = await axios.post<IResponseType>(`${USER_API_URL}/auth/forgot-password/reset-password`, payload);
     return response.data;
 };
+
+export const getMyProfileService = async (): Promise<IResponseType> => {
+    const response = await axios.get<IResponseType>(`${USER_API_URL}/profile/me`, headers(AUTH_TOKEN));
+    return response.data;
+}
