22b56896ed08c7fae09ab72b6dba832263a5a08c
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-15 17:27:28 +0700

    Update: remove logout parameter

--- client/src/hooks/auth/useLogout.ts
+++ client/src/hooks/auth/useLogout.ts
@@ -1,10 +1,9 @@
-import { ILogoutType } from "@/types/authTypes";
 import { logoutService } from "@/services/userService";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 export const useLogout = () => {
-    return useMutation<unknown, AxiosError, ILogoutType>({
-        mutationFn: (data: ILogoutType) => logoutService(data)
+    return useMutation<unknown, AxiosError>({
+        mutationFn: () => logoutService()
     });
 }
