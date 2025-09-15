65570b3472876eb702b3c5a7490c6ddd6ba6d43d
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-15 17:30:34 +0700

    Update: add validation if success message is similar

--- client/src/hooks/useSuccessToast.ts
+++ client/src/hooks/useSuccessToast.ts
@@ -1,14 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useToast } from '@/hooks/useToast';
 import { getDataResponseMessage } from '@/utils/getDataResponse';
 
 const useSuccessToast = (isSuccess: boolean, data: any) => {
     const { toastSuccess } = useToast();
+    const successRef = useRef<string | null>(null);
 
     useEffect(() => {
         if (isSuccess && data) {
-            toastSuccess(getDataResponseMessage(data) || 'Operasi berhasil');
+            const currentSuccess = getDataResponseMessage(data) || 'Operasi berhasil';
+            if (successRef.current !== currentSuccess) {
+                toastSuccess(getDataResponseMessage(data) || 'Operasi berhasil');
+                successRef.current = currentSuccess;
+            }
+            if (!isSuccess) {
+                successRef.current = null;
+            }
         }
     }, [isSuccess, data, toastSuccess]);
 
