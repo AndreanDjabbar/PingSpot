34e429c85d44661e1bb81505423db80c0f9b80dd
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-18 17:01:02 +0700

    Add: add report model

---
+++ server/internal/model/mainService/report.go
@@ -0,0 +1,23 @@
+package mainservice
+
+import "server/internal/model/user"
+
+type ReportType string
+
+const (
+	Infrastructure ReportType = "INFRASTRUCTURE"
+	Environment    ReportType = "ENVIRONMENT"
+	SocialIssue    ReportType = "SOCIAL_ISSUE"
+	Safety		   ReportType = "SAFETY"
+	Other		   ReportType = "OTHER"
+)
+
+type Report struct {
+	ID        uint   	`gorm:"primaryKey"`
+	UserID    uint   	`gorm:"not null"`
+	User      user.User `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	ReportTitle  string `gorm:"size:255;not null"`
+	ReportType   ReportType `gorm:"type:varchar(30);not null"`
+	ReportDescription string `gorm:"type:text;not null"`
+	CreatedAt int64  `gorm:"autoCreateTime"`
+}
