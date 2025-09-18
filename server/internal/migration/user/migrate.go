966c316085b01bc4fa1a97211490ab98342228c8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-18 17:03:04 +0700

    Add: add create report, report image, and report location table migration

--- server/internal/migration/user/migrate.go
+++ server/internal/migration/user/migrate.go
@@ -1,7 +1,8 @@
 package user
 
 import (
-	"server/internal/model/user"
+	mainModel "server/internal/model/mainService"
+	userModel "server/internal/model/user"
 	"server/pkg/logger"
 
 	"github.com/go-gormigrate/gormigrate/v2"
@@ -14,23 +15,23 @@ func Migrate(db *gorm.DB) error {
 		{
 			ID: "24072025_initial_migration",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&user.User{})
+				return tx.AutoMigrate(&userModel.User{})
 			},
 			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&user.User{})
+				return tx.Migrator().DropTable(&userModel.User{})
 			},
 		},
 		{
 			ID: "29082025_remove_phone_field",
 			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasColumn(&user.User{}, "phone") {
-					return tx.Migrator().DropColumn(&user.User{}, "phone")
+				if tx.Migrator().HasColumn(&userModel.User{}, "phone") {
+					return tx.Migrator().DropColumn(&userModel.User{}, "phone")
 				}
 				return nil
 			},
 			Rollback: func(tx *gorm.DB) error {
-				if !tx.Migrator().HasColumn(&user.User{}, "phone") {
-					return tx.Migrator().AddColumn(&user.User{}, "phone")
+				if !tx.Migrator().HasColumn(&userModel.User{}, "phone") {
+					return tx.Migrator().AddColumn(&userModel.User{}, "phone")
 				}
 				return nil
 			},
@@ -38,23 +39,23 @@ func Migrate(db *gorm.DB) error {
 		{
 			ID: "29082025_add_user_profile_table",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&user.UserProfile{})
+				return tx.AutoMigrate(&userModel.UserProfile{})
 			},
 			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&user.UserProfile{})
+				return tx.Migrator().DropTable(&userModel.UserProfile{})
 			},
 		},
 		{
 			ID: "04092025_rename_avatar_to_profile_picture",
 			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasColumn(&user.UserProfile{}, "avatar") {
-					return tx.Migrator().RenameColumn(&user.UserProfile{}, "avatar", "profile_picture")
+				if tx.Migrator().HasColumn(&userModel.UserProfile{}, "avatar") {
+					return tx.Migrator().RenameColumn(&userModel.UserProfile{}, "avatar", "profile_picture")
 				}
 				return nil
 			},
 			Rollback: func(tx *gorm.DB) error {
-				if tx.Migrator().HasColumn(&user.UserProfile{}, "profile_picture") {
-					return tx.Migrator().RenameColumn(&user.UserProfile{}, "profile_picture", "avatar")
+				if tx.Migrator().HasColumn(&userModel.UserProfile{}, "profile_picture") {
+					return tx.Migrator().RenameColumn(&userModel.UserProfile{}, "profile_picture", "avatar")
 				}
 				return nil
 			},
@@ -62,14 +63,14 @@ func Migrate(db *gorm.DB) error {
 		{
 			ID: "11092025_remove_age_field",
 			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasColumn(&user.UserProfile{}, "age") {
-					return tx.Migrator().DropColumn(&user.UserProfile{}, "age")
+				if tx.Migrator().HasColumn(&userModel.UserProfile{}, "age") {
+					return tx.Migrator().DropColumn(&userModel.UserProfile{}, "age")
 				}
 				return nil
 			},
 			Rollback: func(tx *gorm.DB) error {
-				if !tx.Migrator().HasColumn(&user.UserProfile{}, "age") {
-					return tx.Migrator().AddColumn(&user.UserProfile{}, "age")
+				if !tx.Migrator().HasColumn(&userModel.UserProfile{}, "age") {
+					return tx.Migrator().AddColumn(&userModel.UserProfile{}, "age")
 				}
 				return nil
 			},
@@ -77,14 +78,56 @@ func Migrate(db *gorm.DB) error {
 		{
 			ID: "11092025_add_birthday_field",
 			Migrate: func(tx *gorm.DB) error {
-				if !tx.Migrator().HasColumn(&user.UserProfile{}, "birthday") {
-					return tx.Migrator().AddColumn(&user.UserProfile{}, "birthday")
+				if !tx.Migrator().HasColumn(&userModel.UserProfile{}, "birthday") {
+					return tx.Migrator().AddColumn(&userModel.UserProfile{}, "birthday")
 				}
 				return nil
 			},
 			Rollback: func(tx *gorm.DB) error {
-				if tx.Migrator().HasColumn(&user.UserProfile{}, "birthday") {
-					return tx.Migrator().DropColumn(&user.UserProfile{}, "birthday")
+				if tx.Migrator().HasColumn(&userModel.UserProfile{}, "birthday") {
+					return tx.Migrator().DropColumn(&userModel.UserProfile{}, "birthday")
+				}
+				return nil
+			},
+		},
+		{
+			ID: "18092025_add_report_table",
+			Migrate: func(tx *gorm.DB) error {
+				return tx.AutoMigrate(&mainModel.Report{})
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return tx.Migrator().DropTable(&mainModel.Report{})
+			},
+		},
+		{
+			ID: "18092025_add_report_image_table",
+			Migrate: func(tx *gorm.DB) error {
+				return tx.AutoMigrate(&mainModel.ReportImage{})
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return tx.Migrator().DropTable(&mainModel.ReportImage{})
+			},
+		},
+		{
+			ID: "18092025_add_report_location_table",
+			Migrate: func(tx *gorm.DB) error {
+				return tx.AutoMigrate(&mainModel.ReportLocation{})
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return tx.Migrator().DropTable(&mainModel.ReportLocation{})
+			},
+		},
+		{
+			ID: "18092025_add_road_field",
+			Migrate: func(tx *gorm.DB) error {
+				if !tx.Migrator().HasColumn(&mainModel.ReportLocation{}, "road") {
+					return tx.Migrator().AddColumn(&mainModel.ReportLocation{}, "road")
+				}
+				return nil
+			},
+			Rollback: func(tx *gorm.DB) error {
+				if tx.Migrator().HasColumn(&mainModel.ReportLocation{}, "road") {
+					return tx.Migrator().DropColumn(&mainModel.ReportLocation{}, "road")
 				}
 				return nil
 			},
