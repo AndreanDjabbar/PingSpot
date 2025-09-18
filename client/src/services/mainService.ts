185cfbc700e5db5ffb6051be51078d760be75961
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-18 16:54:03 +0700

    Add: add report service caller

--- client/src/services/mainService.ts
+++ client/src/services/mainService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { IReverseLocationType } from "@/types/mainTypes";
+import getAuthToken from "@/utils/getAuthToken";
 
 type IResponseType = {
     message: string;
@@ -7,9 +8,36 @@ type IResponseType = {
 }
 
 const REVERSE_LOCATION_API_URL = `${process.env.NEXT_PUBLIC_REVERSE_LOCATION_URL}`;
+const MAIN_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/main`;
+
+// const COMMON_HEADERS = (authToken: string) => {
+//     return {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//             'Authorization': `Bearer ${authToken}`
+//         }
+//     }
+// }
+
+const MULTIPART_HEADERS = (authToken: string) => {
+    return {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${authToken}`
+        }
+    }
+}
 
 export const reverseCurrentLocationService = async (payload: IReverseLocationType): 
 Promise<IResponseType> => {
     const response = await axios.get<IResponseType>(`${REVERSE_LOCATION_API_URL}?lat=${payload.latitude}&lon=${payload.longitude}&format=json`);
     return response.data;
 };
+
+export const createReportService = async (payload: FormData): Promise<IResponseType> => {
+    const authToken = getAuthToken();
+    const response = await axios.post<IResponseType>(`${MAIN_API_URL}/report`, payload, MULTIPART_HEADERS(authToken || ''));
+    return response.data;
+}
