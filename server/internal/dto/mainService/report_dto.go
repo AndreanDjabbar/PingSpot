2e81e1019a17b483f5e78893a8a9d5b9826c4011
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-09-18 17:00:27 +0700

    Add: add create report DTO

---
+++ server/internal/dto/mainService/report_dto.go
@@ -0,0 +1,26 @@
+package mainservice
+
+type CreateReportRequest struct {
+	ReportTitle       string   `json:"reportTitle" validate:"required";max=200"`
+	ReportType        string   `json:"reportType" validate:"required,oneof=INFRASTRUCTURE ENVIRONMENT SOCIAL_ISSUE SAFETY OTHER"`
+	ReportDescription string   `json:"reportDescription" validate:"required"`
+	DetailLocation    string   `json:"detailLocation" validate:"required"`
+	Latitude          float64  `json:"latitude" validate:"required"`
+	Longitude         float64  `json:"longitude" validate:"required"`
+	DisplayName       *string  `json:"displayName" validate:"omitempty,max=255"`
+	AddressType       *string  `json:"addressType" validate:"omitempty,max=100"`
+	Country           *string  `json:"country" validate:"omitempty,max=100"`
+	CountryCode       *string  `json:"countryCode" validate:"omitempty,max=10"`
+	Region            *string  `json:"region" validate:"omitempty,max=100"`
+	PostCode          *string  `json:"postCode" validate:"omitempty,max=20"`
+	County            *string  `json:"county" validate:"omitempty,max=200"`
+	State             *string  `json:"state" validate:"omitempty,max=200"`
+	Road			  *string  `json:"road" validate:"omitempty,max=200"`
+	Village         	*string  `json:"village" validate:"omitempty,max=200"`
+	Suburb            *string  `json:"suburb" validate:"omitempty,max=200"`
+	Image1URL       	*string  `json:"image1Url" validate:"omitempty,max=255"`
+	Image2URL       	*string  `json:"image2Url" validate:"omitempty,max=255"`
+	Image3URL       	*string  `json:"image3Url" validate:"omitempty,max=255"`
+	Image4URL       	*string  `json:"image4Url" validate:"omitempty,max=255"`
+	Image5URL       	*string  `json:"image5Url" validate:"omitempty,max=255"`
+}
