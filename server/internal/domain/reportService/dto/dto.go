2b0e5e0da56e9f8c26c02ef41da96480dce486df
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-11-01 14:34:15 +0700

    Add: add dto type

---
+++ server/internal/domain/reportService/dto/dto.go
@@ -0,0 +1,77 @@
+package dto
+
+type TotalReportCount struct {
+	TotalReports               int64 `json:"totalReports"`
+	TotalInfrastructureReports int64 `json:"totalInfrastructureReports"`
+	TotalEnvironmentReports    int64 `json:"totalEnvironmentReports"`
+	TotalSafetyReports         int64 `json:"totalSafetyReports"`
+	TotalTrafficReports        int64 `json:"totalTrafficReports"`
+	TotalPublicFacilityReports int64 `json:"totalPublicFacilityReports"`
+	TotalWasteReports          int64 `json:"totalWasteReports"`
+	TotalWaterReports          int64 `json:"totalWaterReports"`
+	TotalElectricityReports    int64 `json:"totalElectricityReports"`
+	TotalHealthReports         int64 `json:"totalHealthReports"`
+	TotalSocialReports         int64 `json:"totalSocialReports"`
+	TotalEducationReports      int64 `json:"totalEducationReports"`
+	TotalAdministrativeReports int64 `json:"totalAdministrativeReports"`
+	TotalDisasterReports       int64 `json:"totalDisasterReports"`
+	TotalOtherReports          int64 `json:"totalOtherReports"`
+}
+
+type Report struct {
+	ID                         uint                        `json:"id"`
+	ReportTitle                string                      `json:"reportTitle"`
+	ReportType                 string                      `json:"reportType"`
+	ReportDescription          string                      `json:"reportDescription"`
+	ReportCreatedAt            int64                       `json:"reportCreatedAt"`
+	ReportStatus               string                      `json:"reportStatus"`
+	HasProgress                *bool                       `json:"hasProgress"`
+	UserID                     uint                        `json:"userID"`
+	UserName                   string                      `json:"userName"`
+	FullName                   string                      `json:"fullName"`
+	ProfilePicture             *string                     `json:"profilePicture"`
+	Location                   ReportLocation      `json:"location"`
+	Images                     ReportImage         `json:"images"`
+	TotalReactions             int64                       `json:"totalReactions"`
+	TotalLikeReactions         *int64                      `json:"totalLikeReactions"`
+	TotalDislikeReactions      *int64                      `json:"totalDislikeReactions"`
+	TotalResolvedVotes         *int64                      `json:"totalResolvedVotes"`
+	TotalOnProgressVotes       *int64                      `json:"totalOnProgressVotes"`
+	TotalNotResolvedVotes      *int64                      `json:"totalNotResolvedVotes"`
+	TotalVotes                 int64                       `json:"totalVotes"`
+	IsLikedByCurrentUser       bool                        `json:"isLikedByCurrentUser"`
+	IsDislikedByCurrentUser    bool                        `json:"isDislikedByCurrentUser"`
+	ReportReactions            []ReactReportResponse       `json:"reportReactions"`
+	ReportProgress             []GetProgressReportResponse `json:"reportProgress"`
+	ReportVotes                []GetVoteReportResponse     `json:"reportVotes,omitempty"`
+	IsResolvedByCurrentUser    bool                        `json:"isResolvedByCurrentUser"`
+	IsOnProgressByCurrentUser  bool                        `json:"isOnProgressByCurrentUser"`
+	IsNotResolvedByCurrentUser bool                        `json:"isNotResolvedByCurrentUser"`
+	MajorityVote               *string                     `json:"majorityVote,omitempty"`
+}
+
+type ReportLocation struct {
+	DetailLocation string  `json:"detailLocation"`
+	Latitude       float64 `json:"latitude"`
+	Longitude      float64 `json:"longitude"`
+	DisplayName    *string `json:"displayName"`
+	AddressType    *string `json:"addressType"`
+	Country        *string `json:"country"`
+	CountryCode    *string `json:"countryCode"`
+	Region         *string `json:"region"`
+	Road           *string `json:"road"`
+	PostCode       *string `json:"postCode"`
+	County         *string `json:"county"`
+	State          *string `json:"state"`
+	Village        *string `json:"village"`
+	Suburb         *string `json:"suburb"`
+	Geometry       *string `json:"geometry"`
+}
+
+type ReportImage struct {
+	Image1URL *string `json:"image1URL"`
+	Image2URL *string `json:"image2URL"`
+	Image3URL *string `json:"image3URL"`
+	Image4URL *string `json:"image4URL"`
+	Image5URL *string `json:"image5URL"`
+}
