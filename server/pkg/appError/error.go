e9d4045d2257306b0e2c1a75bc8065e26a81efe5
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-11-23 20:18:50 +0700

    Add: add custom error

---
+++ server/pkg/appError/error.go
@@ -0,0 +1,19 @@
+package apperror
+
+type AppError struct {
+    Code       string `json:"code"`
+    Message    string `json:"message"`
+    StatusCode int    `json:"statusCode"`
+}
+
+func (e *AppError) Error() string {
+    return e.Message
+}
+
+func New(status int, code, message string) *AppError {
+    return &AppError{
+        Code:       code,
+        Message:    message,
+        StatusCode: status,
+    }
+}
