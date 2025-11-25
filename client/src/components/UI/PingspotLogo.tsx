4f645f041a68c0636f1a33b3c639820d821d44c4
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-11-25 16:15:43 +0700

    Add: add pingspot logo props and make new version of logo (white color version)

--- client/src/components/UI/PingspotLogo.tsx
+++ client/src/components/UI/PingspotLogo.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 
-const PingspotLogo = ({
-    type = 'full',
+interface PingspotLogoProps {
+    type?: 'primary' | 'secondary'
+    size?: string | number;
+}
+
+const PingspotLogo: React.FC<PingspotLogoProps> = ({
+    type = 'primary',
     size = '150'
 }) => {
     return (
         <>
-            {type === 'full' 
+            {type === 'secondary' 
             ? (
                 <Image
-                src="/images/pingspot-logo.svg"
+                src="/images/pingspot-icon-white.svg"
                 alt="PingSpot Logo"
                 width={Number(size)}
                 height={Number(size)}
