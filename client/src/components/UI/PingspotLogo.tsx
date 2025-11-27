05212cdd4feb2fd699892a64280fc61f989cf31a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-11-27 11:57:40 +0700

    Update: update pingspot logo url binding

--- client/src/components/UI/PingspotLogo.tsx
+++ client/src/components/UI/PingspotLogo.tsx
@@ -2,34 +2,63 @@ import React from 'react'
 import Image from 'next/image'
 
 interface PingspotLogoProps {
-    type?: 'primary' | 'secondary'
+    variant?: 'primary' | 'secondary'
+    type?: 'full' | 'semi'
     size?: string | number;
 }
 
 const PingspotLogo: React.FC<PingspotLogoProps> = ({
-    type = 'primary',
+    type = 'full',
+    variant = 'primary',
     size = '150'
 }) => {
+
     return (
         <>
-            {type === 'secondary' 
-            ? (
-                <Image
-                src="/images/pingspot-icon-white.svg"
-                alt="PingSpot Logo"
-                width={Number(size)}
-                height={Number(size)}
-                className={`w-[${size}px]`}
-                />
-            )
-            : (
-                <Image
-                src="/images/pingspot-icon.svg"
-                alt="PingSpot Logo"
-                width={Number(size)}
-                height={Number(size)}
-                className={`w-[${size}px]`}
-                />
+            {type === 'semi' ? (
+                <>
+                    {variant === 'primary' 
+                    ? (
+                        <Image
+                        src="/images/pingspot-semi-primary.svg"
+                        alt="PingSpot Logo"
+                        width={Number(size)}
+                        height={Number(size)}
+                        className={`w-[${size}px]`}
+                        />
+                    )
+                    : (
+                        <Image
+                        src="/images/pingspot-semi-secondary.svg"
+                        alt="PingSpot Logo"
+                        width={Number(size)}
+                        height={Number(size)}
+                        className={`w-[${size}px]`}
+                        />
+                    )}
+                </>
+            ) : (
+                <>
+                    {variant === 'primary' 
+                    ? (
+                        <Image
+                        src="/images/pingspot-full-primary.svg"
+                        alt="PingSpot Logo"
+                        width={Number(size)}
+                        height={Number(size)}
+                        className={`w-[${size}px]`}
+                        />
+                    )
+                    : (
+                        <Image
+                        src="/images/pingspot-full-secondary.svg"
+                        alt="PingSpot Logo"
+                        width={Number(size)}
+                        height={Number(size)}
+                        className={`w-[${size}px]`}
+                        />
+                    )}
+                </>
             )}
         </>
     )
