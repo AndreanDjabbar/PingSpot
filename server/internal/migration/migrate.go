558ce3725a78d0ceb2a621e53c2e81ef60c9dfc4
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-12-04 17:57:24 +0700

    Update: reset migrations

--- server/internal/migration/migrate.go
+++ server/internal/migration/migrate.go
@@ -29,24 +29,6 @@ func Migrate(db *gorm.DB) error {
 				return tx.Migrator().DropTable(&model.User{})
 			},
 		},
-		{
-			ID: "11212025_add_is_deleted_field_to_report",
-			Migrate: func(tx *gorm.DB) error {
-				return tx.Migrator().AddColumn(&model.Report{}, "is_deleted")
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropColumn(&model.Report{}, "is_deleted")
-			},
-		},
-		{
-			ID: "11212025_add_deleted_at_field_to_report",
-			Migrate: func(tx *gorm.DB) error {
-				return tx.Migrator().AddColumn(&model.Report{}, "deleted_at")
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropColumn(&model.Report{}, "deleted_at")
-			},
-		},
 	})
 
 	err := m.Migrate()
