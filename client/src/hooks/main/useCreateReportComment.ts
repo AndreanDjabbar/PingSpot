2285d918ba19b7ad02cf12d93094e33c6abfda79
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-12-07 15:01:06 +0700

    Add: add invalidate queries on success

--- client/src/hooks/main/useCreateReportComment.ts
+++ client/src/hooks/main/useCreateReportComment.ts
@@ -1,5 +1,5 @@
 import { createReportCommentService } from "@/services/mainService"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { ICreateReportCommentResponse } from "@/types/api/report"
 
 interface ICreateReportCommentProps {
@@ -8,7 +8,11 @@ interface ICreateReportCommentProps {
 }
 
 export const useCreateReportCommentReport = () => {
+    const queryClient = useQueryClient();
     return useMutation<ICreateReportCommentResponse, Error, ICreateReportCommentProps>({
-        mutationFn: (data: ICreateReportCommentProps) => createReportCommentService(data.reportID, data.data)
+        mutationFn: (data: ICreateReportCommentProps) => createReportCommentService(data.reportID, data.data),
+        onSuccess: (data, variables) => {
+            queryClient.invalidateQueries({queryKey: ['report', variables.reportID, 'comments']});
+        },
     })
 }
