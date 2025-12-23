30c2f80bc2df0c29e8d4e5a00351ab82307ea1e4
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-12-23 20:24:40 +0700

    Add: register search router

--- server/internal/router/router.go
+++ server/internal/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	authRouter "server/internal/domain/authService/router"
 	mainRouter "server/internal/domain/reportService/router"
+	searchRouter "server/internal/domain/searchService/router"
 	userRouter "server/internal/domain/userService/router"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,5 +12,6 @@ import (
 func RegisterRoutes(app *fiber.App) {
 	userRouter.RegisterUserRoutes(app)
 	authRouter.RegisterAuthRoutes(app)
+	searchRouter.RegisterSearchRoutes(app)
 	mainRouter.RegisterReportRoutes(app)
 }
