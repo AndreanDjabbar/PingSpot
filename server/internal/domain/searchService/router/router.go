7d68089cf49849128d9fd7618f3db3c328fd74a3
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-12-23 20:25:02 +0700

    Add: add search router

---
+++ server/internal/domain/searchService/router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	reportRepository "server/internal/domain/reportService/repository"
+	"server/internal/domain/searchService/handler"
+	"server/internal/domain/searchService/service"
+	userRepository "server/internal/domain/userService/repository"
+	"server/internal/infrastructure/database"
+	"server/internal/middleware"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterSearchRoutes(app *fiber.App) {
+	db := database.GetPostgresDB()
+	userRepo := userRepository.NewUserRepository(db)
+	reportRepo := reportRepository.NewReportRepository(db)
+
+	searchService := service.NewSearchService(userRepo, reportRepo)
+	searchHandler := handler.NewSearchHandler(searchService)
+
+	searchRoute := app.Group("/pingspot/api/search", middleware.ValidateAccessToken())
+	searchRoute.Get("/", middleware.TimeoutMiddleware(40*time.Second), searchHandler.HandleSearch)
+}
