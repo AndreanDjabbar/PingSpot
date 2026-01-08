31b35f9f3a0d3014fe008305dda61d4545862a11
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-08 21:34:10 +0700

    Add: add get user statistics hook

--- client/src/hooks/user/index.ts
+++ client/src/hooks/user/index.ts
@@ -2,10 +2,12 @@ import { useLogout } from "./useLogout";
 import { useMyProfile } from "./useMyProfile";
 import { useSaveProfile } from "./useSaveProfile";
 import { useSaveSecurity } from "./useSaveSecurity";
+import { useGetUserStatistics } from "./useGetUserStatistics";
 
 export { 
     useLogout, 
     useMyProfile, 
+    useGetUserStatistics,
     useSaveProfile, 
     useSaveSecurity 
 };
